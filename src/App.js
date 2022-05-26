caef6aff235e1d3543252081df69210291a13221
libregd <libregd@outlook.com>
2022-05-26 22:46:39 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react';
+// import dollar from "./images/dollar.png"
+// import cny from './images/coin.png'
+
+
+const Item = () => {
+
+  const [Items, setItems] = useState([])
+  const [pause, setPause] = useState(0)
+  const state = pause < 3
+  function handleClick(e) {
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      const result = Math.floor(Math.random() * (max - min + 1)) + min;
+      return result % 2
+    }
+    if (pause < 3) {
+      setItems(Items => [...Items, getRandomInt(1, 100)])
+      setPause(pause + 1)
+    }
+    e.currentTarget.classList.toggle('is-flipped');
+
+
+  };
+
+  return (
+    <div className="col col-12">
+
+
+      {pause}/3
+      <progress max="3" value={pause} class="progress-bar">
+      </progress>
+      <br />
+      <div className="d-grid gap-1 d-sm-block">
+        <button onClick={handleClick} className="btn btn-primary " disabled={!state} >dianji{Items[0]}</button>
+      </div>
+      <div class="container">
+        <div class="row row-cols-3 mt-3">
+
+
+        </div>
+      </div>
+      <br />
+      {Items}
+
+
+
+    </div>
+
+
+  )
+}
+
+
+const Group = () => {
+
+  // const [Groups, setGroups] = useState([])
+  return (
+    <div className="row row-cols-3  text-center ">
+
+      <Item />
+
+    </div>
+  )
+}
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container ">
+      <div className="fs-1">hello</div>
+
+      <Group />
+
+
     </div>
   );
 }
